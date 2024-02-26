c068c962a4cd63a21cdaa402d04210fd49515280
killerdroid99 <ajempire09@gmail.com>
2024-02-26 12:53:55 +0530

    Cleaning up unused stuff

--- src/config/config.go
+++ src/config/config.go
@@ -14,13 +14,3 @@ func Setup() (*gorm.DB, error) {
 	}
 	return db, nil
 }
-
-// func RedisClient() *redis.Client {
-// 	client := redis.NewClient(&redis.Options{
-// 		Addr:     "localhost:6379",
-// 		Password: "",
-// 		DB:       0,
-// 	})
-
-// 	return client
-// }
